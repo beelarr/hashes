01ebdb5101694b5afc3a6fc1d204a079f219597f
beelarr <bryonl@me.com>
2017-10-03 18:00:54 -0500

    Building Exercise

---
+++ stocks.rb
@@ -0,0 +1,4 @@
+stock_dict = { 'APL' => 'Apple', 'GOOGL' => 'Google', 'FB' => 'Facebook' }
+
+purchases = [['APL', 100, '3-oct-2017', 48], ['GOOGL', 100, '3-oct-2017', 330],
+             ['FB', 100, '3-oct-2017', 432]]
